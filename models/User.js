7d742e7c234f817374a8ab52a1d7ece024e115fb
lawrence.webon <lawrencewebon@gmail.com>
2022-10-28 11:05:01 +0800

    Updated add activity code.

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose= require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: [true, "Firstname is Required!"]
+    },
+    lastname: {
+        type: String,
+        required: [true, "Lastname is Required!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is Required!"]
+    },
+    Password: {
+        type: String,
+        required: [true, "Email is Required!"]
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    mobileNo:{
+        type: String,
+        required: [true, "Mobile number is Required!"]
+    },
+    enrollments: [{
+        courseid: {
+            type: String,
+            required: [true, "Course ID is required!"]
+        },
+        enrolledOn:{
+            type: Date,
+            default: new Date()
+        },
+        status:{
+            type: String,
+            default: "Enrolled"
+        }
+    }]
+
+
+})
+
+module.exports = mongoose.model("User", userSchema);
